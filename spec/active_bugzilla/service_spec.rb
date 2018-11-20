fd7a811cf59137f5723069c76a19ef4cee402247
Nick LaMuro <nicklamuro@gmail.com>
2018-11-19 19:26:59 -0600

    Use FakeBugzillaServer in Service specs

--- spec/active_bugzilla/service_spec.rb
+++ spec/active_bugzilla/service_spec.rb
@@ -1,7 +1,10 @@
+# rubocop:disable Style/NumericLiterals
 require 'spec_helper'
 
-describe ActiveBugzilla::Service do
-  let(:bz) { described_class.new("http://uri.to/bugzilla", "calvin", "hobbes") }
+describe ActiveBugzilla::Service, :include_bugzilla_server_helper do
+  let(:host) { "http://#{FakeBugzillaServer.server_host}" }
+  let(:port) { FakeBugzillaServer.server_port }
+  let(:bz)   { described_class.new(host, "calvin", "hobbes", :port => port) }
 
   context "#new" do
     it 'normal case' do
@@ -31,29 +34,41 @@ describe ActiveBugzilla::Service do
     end
 
     it "when the specified bug does not exist" do
-      output = {}
-
-      allow(::XMLRPC::Client).to receive(:new)
-        .and_return(double('xmlrpc_client', :call => output, :set_parser => nil))
+      with_a_bz_server_definition do
+        action("Bug.get") do
+          assert_bugzilla_auth "calvin", "hobbes"
+          assert_params        "ids" => [94897099]
+          {}
+        end
+      end
       matches = bz.get(94897099)
+      stop_bz_server
+
       expect(matches).to be_kind_of(Array)
       expect(matches).to be_empty
     end
 
     it "when producing valid output" do
-      output = {
-        'bugs' => [
-          {
-            "priority" => "unspecified",
-            "keywords" => ["ZStream"],
-            "cc"       => ["calvin@redhat.com", "hobbes@RedHat.com"],
-          },
-        ]
-      }
+      with_a_bz_server_definition do
+        action "Bug.get" do
+          assert_bugzilla_auth "calvin", "hobbes"
+          assert_params        "ids" => ["948972"]
 
-      allow(::XMLRPC::Client).to receive(:new)
-        .and_return(double('xmlrpc_client', :call => output, :set_parser => nil))
+          {
+            'bugs' => [
+              {
+                "priority" => "unspecified",
+                "keywords" => ["ZStream"],
+                "cc"       => ["calvin@redhat.com", "hobbes@RedHat.com"],
+              },
+            ]
+          }
+        end
+      end
+
       existing_bz = bz.get("948972").first
+
+      stop_bz_server
 
       expect(bz.last_command).to include("Bug.get")
 
@@ -64,6 +79,135 @@ describe ActiveBugzilla::Service do
   end
 
   context "#clone" do
+    let(:commenter_1)      { "Calvin@redhat.com" }
+    let(:commenter_2)      { "Hobbes@redhat.com" }
+    let(:overwrite_params) { {} }
+    let(:existing_bz) do
+      {
+        "id"             => 948972,
+        "description"    => "Description of problem:\n\nIt's Broken",
+        "priority"       => "unspecified",
+        "assigned_to"    => "calvin@redhat.com",
+        "target_release" => ["---"],
+        "keywords"       => ["ZStream"],
+        "cc"             => ["calvin@redhat.com", "hobbes@RedHat.com"],
+        "comments"       => [
+          {
+            "is_private"    => false,
+            "count"         => 0,
+            "time"          => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
+            "bug_id"        => 948970,
+            "author"        => commenter_1,
+            "text"          => "It's Broken and impossible to reproduce",
+            "creation_time" => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
+            "id"            => 5777871,
+            "creator_id"    => 349490
+          },
+          {
+            "is_private"    => false,
+            "count"         => 1,
+            "time"          => XMLRPC::DateTime.new(1970, 11, 10, 16, 18, 30),
+            "bug_id"        => 948970,
+            "author"        => commenter_2,
+            "text"          => "Fix Me Now!",
+            "creation_time" => XMLRPC::DateTime.new(1972, 2, 14, 0, 0, 0),
+            "id"            => 5782170,
+            "creator_id"    => 349490
+          },
+        ]
+      }
+    end
+
+    # Note:  For the first comment's `:creation_time`, this ends up being `nil`
+    # on older versions of ruby do to the fact that the `to_time` method used to
+    # call out to `Time.gm(XMLRPC::DateTime#to_a)`, and in pre Ruby 2.0, this
+    # would return `nil` if the year was less than 1970 (epoch):
+    #
+    #   https://github.com/ruby/ruby/blob/23ccbdf5/lib/xmlrpc/datetime.rb#L113-L119
+    #
+    # This has since changed:
+    #
+    #   https://github.com/ruby/xmlrpc/commit/7a7a3afc
+    #
+    # But for ruby version compatibility, this is calculated again here so it can
+    # be conditionally defined based on Ruby version.
+    let(:comment_1_time) do
+      existing_bz["comments"].first["creation_time"].to_time # rubocop:disable Rails/Date
+    end
+
+    # rubocop:disable Layout/TrailingWhitespace
+    #
+    # required disable since the string in the model has a space at the end of
+    # the first line
+    let(:clone_description) do
+      <<-BZ_DESCRIPTION.gsub(/^ {8}/, '').chomp
+         +++ This bug was initially created as a clone of Bug ##{existing_bz["id"]} +++ 
+        Description of problem:
+
+        It's Broken
+
+        **********************************************************************
+        Following comment by #{commenter_1} on #{comment_1_time}
+
+
+
+        It's Broken and impossible to reproduce
+
+        **********************************************************************
+        Following comment by #{commenter_2} on 1972-02-14 00:00:00 UTC
+
+
+
+        Fix Me Now!
+      BZ_DESCRIPTION
+    end
+    # rubocop:enable Layout/TrailingWhitespace
+
+    before do
+      fake_bz_server_vars = {
+        :existing_bz       => existing_bz,
+        :overwrite_params  => overwrite_params,
+        :clone_description => clone_description
+      }
+      with_a_bz_server_definition fake_bz_server_vars do
+        action "Bug.get" do
+          if params["ids"] == [94897099]
+            # non-existant BZ
+            #
+            # FIXME:  We probably should re-write this spec, since bugzilla's
+            # xmlrpc seems to throw an error when the BZ doesn't exist, and not
+            # just an empty XML struct.
+            {}
+          else
+            extra_fields = ActiveBugzilla::Service::CLONE_FIELDS.map(&:to_s)
+
+            assert_bugzilla_auth "calvin", "hobbes"
+            assert_params        "ids"            => [948972],
+                                 "include_fields" => extra_fields
+
+            { "bugs" => [existing_bz] }
+          end
+        end
+
+        action "Bug.create" do
+          expected_params = existing_bz.merge({
+            "cf_clone_of"        => existing_bz["id"],
+            "description"        => clone_description,
+            "comment_is_private" => false
+          }.merge(overwrite_params))
+          expected_params.delete("comments")
+          expected_params.delete("id")
+
+          assert_bugzilla_auth "calvin", "hobbes"
+          assert_params        expected_params
+
+          { "id" => 948992 }
+        end
+      end
+    end
+
+    after { stop_bz_server }
+
     it "when no argument is specified" do
       expect { bz.clone }.to raise_error(ArgumentError)
     end
@@ -73,102 +217,38 @@ describe ActiveBugzilla::Service do
     end
 
     it "when the specified bug to clone does not exist" do
-      output        = {}
-      error_message = "no BZ with id 94897099 found!"
-
-      allow(::XMLRPC::Client).to receive(:new).and_return(double('xmlrpc_client', :call => output, :set_parser => nil))
-      expect { bz.clone(94897099) }.to raise_error ActiveBugzilla::Bug::NotFound, error_message
+      expect { bz.clone(94897099) }.to raise_error ActiveBugzilla::Bug::NotFound
     end
 
-    it "when producing valid output" do
-      output = {"id" => 948992}
-      existing_bz = {
-        "description"    => "Description of problem:\n\nIt's Broken",
-        "priority"       => "unspecified",
-        "assigned_to"    => "calvin@redhat.com",
-        "target_release" => ["---"],
-        "keywords"       => ["ZStream"],
-        "cc"             => ["calvin@redhat.com", "hobbes@RedHat.com"],
-        "comments"       => [
-          {
-            "is_private"    => false,
-            "count"         => 0,
-            "time"          => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
-            "bug_id"        => 948970,
-            "author"        => "Calvin@redhat.com",
-            "text"          => "It's Broken and impossible to reproduce",
-            "creation_time" => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
-            "id"            => 5777871,
-            "creator_id"    => 349490
-          },
-          {
-            "is_private"    => false,
-            "count"         => 1,
-            "time"          => XMLRPC::DateTime.new(1970, 11, 10, 16, 18, 30),
-            "bug_id"        => 948970,
-            "author"        => "Hobbes@redhat.com",
-            "text"          => "Fix Me Now!",
-            "creation_time" => XMLRPC::DateTime.new(1972, 2, 14, 0, 0, 0),
-            "id"            => 5782170,
-            "creator_id"    => 349490
-          },
-        ]
-      }
+    context "when producing valid output" do
+      it "creates the new BZ and returns the new BZ ID" do
+        new_bz_id = bz.clone(existing_bz["id"])
+        expect(bz.last_command).to include("Bug.create")
 
-      expect(bz).to receive(:get).and_return([existing_bz])
-      allow(::XMLRPC::Client).to receive(:new)
-        .and_return(double('xmlrpc_create', :call => output, :set_parser => nil))
-      new_bz_id = bz.clone("948972")
-
-      expect(bz.last_command).to include("Bug.create")
-
-      expect(new_bz_id).to eq(output["id"])
+        expect(new_bz_id).to eq(948992)
+      end
     end
 
-    it "when providing override values" do
-      output = {"id" => 948992}
-      existing_bz = {
-        "description"    => "Description of problem:\n\nIt's Broken",
-        "priority"       => "unspecified",
-        "assigned_to"    => "calvin@redhat.com",
-        "target_release" => ["---"],
-        "keywords"       => ["ZStream"],
-        "cc"             => ["calvin@redhat.com", "hobbes@RedHat.com"],
-        "comments"       => [
-          {
-            "is_private"    => false,
-            "count"         => 0,
-            "time"          => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
-            "bug_id"        => 948970,
-            "author"        => "Buzz.Aldrin@redhat.com",
-            "text"          => "It's Broken and impossible to reproduce",
-            "creation_time" => XMLRPC::DateTime.new(1969, 7, 20, 16, 18, 30),
-            "id"            => 5777871,
-            "creator_id"    => 349490
-          },
-          {
-            "is_private"    => false,
-            "count"         => 1,
-            "time"          => XMLRPC::DateTime.new(1970, 11, 10, 16, 18, 30),
-            "bug_id"        => 948970,
-            "author"        => "Neil.Armstrong@redhat.com",
-            "text"          => "Fix Me Now!",
-            "creation_time" => XMLRPC::DateTime.new(1972, 2, 14, 0, 0, 0),
-            "id"            => 5782170,
-            "creator_id"    => 349490
-          },
-        ]
-      }
+    context "when providing override values" do
+      let(:commenter_1) { "Buzz.Aldrin@redhat.com" }
+      let(:commenter_2) { "Neil.Armstrong@redhat.com" }
 
-      expect(bz).to receive(:get).and_return([existing_bz])
-      allow(::XMLRPC::Client).to receive(:new)
-        .and_return(double('xmlrpc_create', :call => output, :set_parser => nil))
-      new_bz_id = bz.clone("948972", "assigned_to" => "Ham@NASA.gov", "target_release" => ["2.2.0"])
+      let(:overwrite_params) do
+        {
+          "assigned_to"    => "Ham@NASA.gov",
+          "target_release" => ["2.2.0"]
+        }
+      end
 
-      expect(bz.last_command).to include("Bug.create")
+      it "creates the new BZ with the updated params and returns the new ID" do
+        new_bz_id = bz.clone(948972, overwrite_params)
 
-      expect(new_bz_id).to eq(output["id"])
+        expect(bz.last_command).to include("Bug.create")
+
+        expect(new_bz_id).to eq(948992)
+      end
     end
   end
 
 end
+# rubocop:enable Style/NumericLiterals
